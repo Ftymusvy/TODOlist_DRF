c5047239193d53ed003f72af172c16d2fb07476a
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2024-10-20 00:52:06 +0330

    set API and calling that

--- src/task/urls.py
+++ src/task/urls.py
@@ -5,8 +5,10 @@ from task.views import (
     delete_task
 )
 
+
 urlpatterns = [
     path('', show_task, name="show_task"),
     path('task/<int:pk>', TaskDetail.as_view(), name="task_detail"),
     path('<int:id>/delete', delete_task, name="delete_task"),
+    
 ]
