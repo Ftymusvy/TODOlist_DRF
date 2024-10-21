8fb5a9272a8e8d26a7328514c687e2dae99f62f0
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2024-10-21 17:48:16 +0330

    calling(GET) and update(PUT)

--- src/task/api/urls.py
+++ src/task/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from task.api.views import (
     task_list,
+    api_task_detail,
+    api_task_update
 )
 
 urlpatterns = [
-    path('tasks/', task_list, name='task_list')
+    path('tasks/', task_list, name='task_list'),
+    path('task/<int:id>' , api_task_detail , name ='task-detail'),
+    path('task/<int:pk>/update' , api_task_update , name ='task-update')
 ]
--- src/task/api/views.py
+++ src/task/api/views.py
@@ -10,3 +10,33 @@ def task_list(request):
         query = TodoTask.objects.all()
         serializer = TodoTaskSerializer(query, many=True)
         return Response(serializer.data)
+
+
+
+#برگرداندن ابجکت مورد نظر API
+@api_view(['GET'])
+def api_task_detail(request,id):
+    try:
+        task = TodoTask.objects.get(pk=id)
+    except TodoTask.DoesNotExist:
+         return Response({"error": "Task not found or has been deleted"}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializer = TodoTaskSerializer(task)
+        return Response(serializer.data)
+
+        
+#بروزرسانی ابجکت موردنظر با PUT
+@api_view(['PUT'])
+def api_task_update(request,pk):
+    try:
+        task = TodoTask.objects.get(pk=pk)
+    except TodoTask.DoesNotExist:
+          return Response({"error": "Task not found or has been deleted"}, status=status.HTTP_404_NOT_FOUND)
+    data = {}
+    if request.method == "PUT":
+        serializer = TodoTaskSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            data['anjam shod'] = "update anjam shod"
+            return Response(data=data)
+    return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)    
